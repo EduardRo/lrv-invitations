91a112ceabf7f9f229a98b816a7b3d7e297f55d2
EduardRo <eduardstancu@yahoo.com>
2024-01-20 12:57:14 +0200

    users table modified, create roles and types

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,6 +14,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username');
+            $table->enum('role', ['admin', 'business', 'guest'])->default('guest');
+            $table->enum('type', ['individual', 'business'])->default('individual');
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
